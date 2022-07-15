1387c54fa7e3040b41837660cc07158751c658c6
jakenichols2719 <54028990+jakenichols2719@users.noreply.github.com>
2022-07-14 23:33:57 -0700

    Created basic email/authtoken exchange (#2)
    Signed-off-by: jakenichols2719 <jnichols2719@protonmail.com>

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"auth/pkg/authcode"
 	"auth/pkg/ses_mail"
+	"bufio"
+	"fmt"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
 	host := ses_mail.Host{
 		Username: os.Getenv("SES_USERNAME"),
 		Password: os.Getenv("SES_PASSWORD"),
@@ -13,6 +20,19 @@ func main() {
 		Port:     587,
 		Sender:   "jnichols2719@protonmail.com",
 	}
-	msg := ses_mail.NewAuthMessage("jani9652@colorado.edu", "AAAAAA")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Please enter email to authenticate: ")
+	email, _ := reader.ReadString('\n')
+	code := authcode.NewAuthCode(email)
+	msg := ses_mail.NewAuthMessage("jani9652@colorado.edu", code)
 	ses_mail.SendMessage(host, "jani9652@colorado.edu", msg)
+	fmt.Printf("Please enter authentication code: ")
+	c, _ := reader.ReadString('\n')
+	c = strings.Trim(c, "\r\n")
+	valid := authcode.ValidateAuthCode(email, c)
+	if valid {
+		fmt.Println("Valid!")
+	} else {
+		fmt.Println("Not valid.")
+	}
 }
---
+++ pkg/authcode/authcode.go
@@ -0,0 +1,55 @@
+package authcode
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type AuthCode struct {
+	ForUser string
+	Code    string
+	Created time.Time
+	Expires time.Time
+	Expired bool
+}
+
+var activeCodes map[string]*AuthCode = make(map[string]*AuthCode)
+
+var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func genCode(ct int) string {
+	seq := make([]rune, ct)
+	for i := range seq {
+		seq[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(seq)
+}
+
+func NewAuthCode(forUser string) *AuthCode {
+	now := time.Now()
+	newCode := &AuthCode{
+		ForUser: forUser,
+		Code:    genCode(6),
+		Created: now,
+		Expires: now.Add(time.Minute),
+		Expired: false,
+	}
+	activeCodes[forUser] = newCode
+	return newCode
+}
+
+func ValidateAuthCode(forUser, code string) bool {
+	storedCode, ok := activeCodes[forUser]
+	if !ok {
+		return false
+	}
+	if storedCode.Code == code && storedCode.Expires.After(time.Now()) {
+		delete(activeCodes, forUser)
+		return true
+	} else {
+		fmt.Printf("Invalid: code equality %t, expiry %t", storedCode.Code == code, storedCode.Expires.After(time.Now()))
+		delete(activeCodes, forUser)
+		return false
+	}
+}
